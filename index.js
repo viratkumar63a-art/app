bc53ccbb0b621c27ce6adf7f332a5af50780ce58
viratkumar63a-art <viratkumar63a@gmail.com>
2025-10-22 09:10:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,430 @@
+//variables:-
+// variables are the containers which store data values
+/*Multi
+line
+comment
+*/
+var number1 = 34;
+var number2 = 43;
+console.log(number1 + number2);
+// Data types in java script
+
+
+var number1 = 7;
+var number2 = 6.7;
+var name = "||helo";
+console.log(number1 + number2 + name);
+
+
+// string in java
+// var string1 = "this is string1 ";
+// var string2 = 'this is string2'
+
+
+// const variables in java
+
+// const number = 5;
+// console.log(number);
+// if else condition
+var num1 = 23;
+if (num1 > 23)
+    console.log("hello");
+//  else
+//      console.log(pageYOffset, indeed)
+else if (num1 > 14 && num1 < 20);
+console.log("between");
+
+
+// let variables in java ;it is locally
+// we can change the variable name in let but not in const
+// let name = "sundar";
+// name = "virat";
+// console.log(name);
+
+// addition of  three number
+var a = 34; b = 20; c = 32;
+var add = (a + b + c);
+console.log(add);
+
+// let a = 20;
+// b = "helo";
+// let add = (a + b);
+// console.log(add)
+
+// let a = 10; b = 20; c = 30;
+// let add = (a + b + c);
+// console.log(add)
+
+// bool in java
+
+// a = 7
+// a == 7;
+// console.log(a == 7);
+
+
+// object in java
+
+// const student = {
+//     name: "virat",
+//     roll: 17,
+//     branch: "cse"
+// }
+// console.log(student)
+
+// if else statement in java script
+// var age = 7;
+// if (age > 15) {
+//     console.log("you can drink");
+// }
+// else; {
+//     console.log("can't drink");
+// }
+
+// if else ladder
+// var age = 25;
+// if (age > 20);
+// {
+//     console.log("he is good")
+// }
+// elseif(age > 18);
+// {
+//     console.log("can vote");
+// }
+// elseif(age > 15);
+// {
+//     console.log("not a child");
+// }
+// elseif(age > 10);
+// {
+//     console.log("you can play");
+// }
+// else;{
+//     console.log("bacho ho");
+// }
+// console.log("end of ladder");
+
+// array in javascript
+
+
+// var arr = [1, 2, 3, 4, 5, 6, 7]
+// console.log(arr);
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+// array iteration from for each
+// var array=[1,2,3,4,5,6,7]
+// arr.forEach(funtion(element){
+//     console.log(element);
+// })
+
+// while loop java script
+
+// var arr = [1, 2, 3, 4, 5, 6, 7]
+// let j = 0;
+// while (j < arr.length) {
+//     console.log(arr[j]);
+//     j++;
+// }
+// var arr = [1, 2, 3, 4, 5, 6, 7]
+// let j = 0;
+// do {
+//     console.log(arr[j]);
+//     j++;
+// } while (j < arr.length);
+
+// break in java script:-
+
+// var arr = [1, 2, 3, 4, 5, 6, 7]
+// for (var i = 0; i < arr.length; i++) {
+//     if (i == 2) {
+//         break;
+//     }
+//     console.log(arr[i])
+// }
+
+// continue in java script
+// var arr = [1, 2, 3, 4, 5, 6, 7]
+// for (var i = 0; i < arr.length; i++) {
+//     if (i == 4) {
+//         continue;
+//     }
+//     console.log(arr[i])
+// }
+// length in java script
+// let Myarr = ["orange", 22, 33, 33, 3, 333, "mango"]
+// console.log(Myarr.length)
+// Myarr.pop()
+// pop function ek element hata deta hai last se
+// Myarr.push("virat")
+// push function ek element add kardeta hai
+// Myarr.shift()
+// shift pahla element hata deta hai
+// Myarr.unshift("virat")
+// unshift se pahla element aap jo dena chahte hain wo aa aajaega
+// const newLen = Myarr.unshift("virat")
+
+// console.log(newLen)
+// ek Element badha kar array print karega
+// Myarr.sort()
+
+// alphabetically sort kar deta hai
+// console.log(Myarr);
+// let mynewstring = "Hi I am Virat"
+// to find length of the string
+// console.log(mynewstring.length)
+
+// console.log(mynewstring.indexOf("virat"))
+// ye function batata hai kis index pe aapka string hai
+
+// console.log(mynewstring.slice(1, 5))
+// ye slice function batata hai jo kaha se kaha tak print karana hai
+// d = mynewstring.replace("hi", "hello");
+// d = d.replace("Hi", "hello");
+// console.log(d, mynewstring)
+// yaha hi hello me replace ho jaega
+
+// let myDate = new Date();
+// console.log(myDate.getTime());
+// console.log(myDate.getfullyear());
+// console.log(myDate.getday());
+// console.log(myDate.getMinutes());
+// s
+// m
+// t
+// w
+// t
+// f
+// s
+
+// airthmetic operator
+
+// let a = 20, b = 10;
+// add = a + b;
+// sub = a - b;
+// multiply = a * b;
+// divide = a / b;
+// console.log(add)
+// console.log(sub)
+// console.log(multiply)
+// console.log(divide)
+
+// pre/post increment
+// let a = 10;
+// console.log(++a)
+// console.log(a++)
+
+// pre/post decrement
+
+// let a = 10;
+// // console.log(--a)
+// // console.log(a)
+// console.log(a--)
+// console.log(a)
+
+// assignment operator comaparision operator
+// let a = 10, b = 20;
+// console.log(a + b);
+// console.log(a += b);
+// console.log(a -= b);
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a /= b);
+// console.log(a === b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+// logical operator
+// let a = 10, b = 20;
+// console.log(a > 5 && b > 10);
+// console.log(a > 3 || b > 4);
+// console.log(a > 2 != b > 5);
+
+
+{
+    // console.log("good morning");
+}
+// elseif(time >= 13 && time <= 15);
+{
+    // console.log("good evening");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
